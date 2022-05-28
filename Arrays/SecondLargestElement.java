848f6dd9825540a06b9c88faabedde739b5fd6be
Deepak <saideepak220@gmail.com>
2022-05-28 20:26:28 +0530

    commit

---
+++ Arrays/SecondLargestElement.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class SecondLargestElement {
+    public static void main(String[] args){
+        int[] arr = {1,1,1,1,0};
+        int secondLargest = getSecondLargest(arr);
+        System.out.println(secondLargest);
+    }
+
+    public static int getSecondLargest(int[] arr){
+        int first;
+        int second = -1;
+        if(arr.length < 2) return -1;
+        first=arr[0];
+        for(int i: arr){
+            if(i>first){
+                second = first;
+                first = i;
+            }else if(i < first){
+                if(second == -1 || i > second){
+                    second = i;
+                }
+            }
+        }
+        return second;
+    }
+}
